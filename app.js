1b9076a5868e4a443ee7bbb3f72938a6f04bfc76
rahul <rahul@roboticwares.com>
2018-08-25 20:36:38 +0530

    task 1 completed

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );      
+app.use(bodyParser.urlencoded({    
+  extended: true
+}));
+var requestData;
+app.get('/', function (req, res) {
+console.log("pass");
+res.status(200);
+res.send();
+});
+app.post('/data', function (req, res) {
+//console.log(req);
+console.log("Request Body : ");
+console.log(req.body);
+requestData=req.body.data;
+res.status(200);
+res.send(req.body);
+});
+
+
+app.get('/data', function (req, res) {
+res.status(200);
+res.send({data:requestData});
+});
+
+
+
+app.listen(3003, function () {
+  console.log('Example app listening on port 3003!');
+});
