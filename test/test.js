640c64c7e0da5d6c63959c2263f5138999a0a63a
prince6893 <princefreak40@gmail.com>
2018-10-19 20:13:46 +0530

    Test Cases

--- test/test.js
+++ test/test.js
@@ -7,6 +7,19 @@ let User = require('../db/Users.js');
 let server = require('../app.js');
 chai.use(chaiHttp);
 // test description
+// Description a group of tests
+describe('My tests', () => {
+    // Description of a specific test
+    describe('dumb test', () => {
+        // The behaviour expected
+        it('should return 4', done => {
+            // what we are actually testing
+            expect(2 + 2).to.equal(4);
+            // when done() is called the test is finished
+            done();
+        })
+    })
+})
 describe('first test', () => {
     // expected behaviour
     it('should return { status: "success" }', done => {
@@ -63,68 +76,54 @@ describe('third test', () => {
         })
     })
 })
-
-// Description a group of tests
-describe('My tests', () => {
-    // Description of a specific test
-    describe('dumb test', () => {
-        // The behaviour expected
-        it('should return 4', done => {
-            // what we are actually testing
-            expect(2 + 2).to.equal(4);
-            // when done() is called the test is finished
-            done();
-        })
-    })
-})
-
+//Task  2 Tests
 describe('Tests for task 2', () => {
-        before(done => {
-            User.destroy({
-                where: {},
-                truncate: true
-            })
-            .then(() => {
-                // After we empty our database we create one user for our login test
-                User.create({
-                    email: 'test@mail.com',
-                    password: '123456'
-                })
-                .then(() => done());
-            });
-    });
+	before(done => {
+		User.destroy({
+			where: {},
+            truncate: true
+		})
+		.then(() => {
+			// After we empty our database we create one user for our login test
+			User.create({
+				email: 'test@mail.com',
+				password: '123456'
+			})
+			.then(() => done());
+		});
+	});
 
-    describe('POST user/signup', () => {
-        it('should sign a user', done => {
+	describe('POST user/signup', () => {
+		it('should sign a user', done => {
 
-            chai.request(server)
-            .post('/user/signup')
-            .send({
-                email: 'test@.com',
-                password: '123456'
-            })
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                expect(res.body).to.be.a('object');
-                expect(res.body).to.have.property('session');
-                done();
-            })
-        })
-    })
-describe('POST user/login', () => {
-    it('should login a user', done => {
-        chai.request(server)
-        .post('/user/login')
-        .send({
-            email: 'test@mail.com',
-            password: '123456'
-        })
-        .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body).to.be.a('object');
-            expect(res.body).to.have.property('session');
-            done();
-        })
-    })
-})
+			chai.request(server)
+			.post('/user/signup')
+			.send({
+				email: 'test@mail.com',
+				password: '123456'
+			})
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.be.a('object');
+				expect(res.body).to.have.property('session');
+				done();
+			})
+		})
+	})
+	describe('POST user/login', () => {
+		it('should login a user', done => {
+			chai.request(server)
+			.post('/user/login')
+			.send({
+				email: 'test@mail.com',
+				password: '123456'
+			})
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.be.a('object');
+				expect(res.body).to.have.property('session');
+				done();
+			})
+		})
+	})
 })
